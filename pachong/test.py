1b37c9210a1064362d1a07c23211301195f5a3f8
tonywuzhiyong <31890457+tonywuzhiyong@users.noreply.github.com>
2017-09-14 21:25:25 +0800

    spider
    这是一个简单的爬虫！

---
+++ pachong/test.py
@@ -0,0 +1,27 @@
+#coding:utf8
+import urllib2
+import cookielib
+import bs4
+
+
+url="http://www.baidu.com"
+print "第一种方法："
+response1=urllib2.urlopen(url)
+print response1.getcode()
+print len(response1.read())
+
+print "第二种方法："
+request=urllib2.Request(url)
+request.add_header("user-agent","Mozilla/5.0")
+response2=urllib2.urlopen(request)
+print response2.getcode()
+print len(response2.read())
+
+print "第三方法："
+cj=cookielib.CookieJar()
+opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+urllib2.install_opener(opener)
+response3=urllib2.urlopen(url)
+print response3.getcode()
+print
+print response3.read()
